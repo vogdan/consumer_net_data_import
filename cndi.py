09d3c796267d8b801dddfb1f1cf716d846871d11
“Bogdan” <dbogdan@gmail.com>
2013-12-03 18:29:25 +0200

    Simplified code and finished task 1

--- cndi.py
+++ cndi.py
@@ -1,112 +1,40 @@
-'''
-creez o matrice care are leg[turile dintre site-uri ca informatie
 
-for line in file
-    put line in dict
-    got_1 = 0
-    for key in dict
-        if dict[key] == 1: 
-            if got_1:
-               matrice[site1, key] += 1 ;# tine cont ca matrice[site1, key] == matrice[key, site1]
-            else:
-                got_1 = 1
-                site1 = key
-            
-'''
 from csv import reader, writer
+from  collections import defaultdict
+from time import clock
 
+start = clock()
 
-class Node:
-
-    def __init__(self, name):
-        self.name = str(name)
-
-    def getName(self):
-        return self.name
-
-    def __str__(self):
-        return self.name
-
- 
-class Link:
-
-    def __init__(self, src, dest, weight=0):
-        self.src = src
-        self.dest = dest
-        self.weight = weight
-
-    def getSource(self):
-        return self.src
-
-    def getDestination(self):
-        return self.dest
-
-    def getWeight(self):
-        return self.weight
-
-    def __str__(self):
-        return str(self.src) + '->' + str(self.dest)
-
-
-class SimpleGraph:
-
-    def __init__(self):
-        self.nodes = set([])
-        self.links = {}
-
-    def addNode(self, node):
-        if node in self.nodes:
-            raise ValueError('Duplicate node')
-        else:
-            self.nodes.add(node)
-            self.links[node] = {}
-
-    def addLink(self, link, weight=1):
-        src = link.getSource()
-        dest = link.getDestination()
-        weight = link.getWeight()
-        if not(src in self.nodes and dest in self.nodes):
-            raise ValueError('Node not in graph')
-        self.links[src][dest]=weight
-
-    def childrenOf(self, node):
-        return self.links[node]
-
-    def hasNode(self, node):
-        return node in self.nodes
-
-    def __str__(self):
-        res = ''
-        for k in self.links:
-            for d in self.links[k]:
-                res = res + str(k) + '->' + str(d) + '\n'
-        return res[:-1]
-
-    def linkWeight(self,node1,node2):
-        if not self.hasNode(node1) or not self.hasNode(node2)or node2 not in self.links[node1]:
-            raise ValueError('Node not in graph')
-        return self.links[node1][node2]
-
-    
+graf = defaultdict(dict)
 
 input_file = "../ExperianBrands.csv"
-graph = SimpleGraph()
+output_file = "output_OneViewSimmons.csv"
 
 with open(input_file, 'rb') as csv_file:
     rdr = reader(csv_file)
     header = rdr.next()
-    for entry in header[1:]:
-        graph.addNode(entry)
-    
+
     for row in rdr:
-        for source in row:
-            if source:
-                src_idx = row.index(source)
-                for destination in row[src_idx:]:
-                    if destination:
-                        dest_idx = row.index(destination)
-                        srcNode = Node(header(src_idx)
-                        destNode = Node(dest_idx)
-                        graph.addLink(Link(srcNode, destNode))
+        one_list = [i for i,x in enumerate(row) if x == '1']
+        for pos in one_list:
+            pos_idx = one_list.index(pos)
+            for other_pos in one_list[pos_idx+1:]:
+                other_pos_idx = one_list.index(other_pos)
+                try:
+                    graf[header[pos]][header[other_pos]] += 1
+                except:
+                    graf[header[pos]][header[other_pos]] = 1
 
 
+with open(output_file, 'w') as out_file:
+    for entry in header[1:]:
+        idx = header.index(entry)
+        for entry2 in header[idx+1:]:
+            try:
+                out_file.write("{},{},{}\n".format(entry, entry2, graf[entry][entry2]))
+            except KeyError:
+                out_file.write("{},{},{}\n".format(entry, entry2, 0))
+
+
+
+print "It took: {} secs".format(clock() - start)
